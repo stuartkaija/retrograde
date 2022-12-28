e431621c16502fcea62de930dbf6e88bb02f4491
Stuart Kaija <stuartkaija@gmail.com>
2022-12-28 09:54:04 -0800

    added notes, updated response arrays

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.scss';
 import Card from './components/Card/Card';
 
+// background for application i.e. image pulled from Nasa goes in here
+
 function App() {
 	return (
 		<div className="App">
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import './Card.scss';
 
+// response arrays - ***this doesn't look very typescript-y right now, research how arrays (consisitng of strings) work in TS
+const inRetrogradeArray = [
+    "Oh no...ohhhhh no...(yes Mercury is in retrograde)",
+    "Yeah. Better not go outside!",
+    "Yes.",
+    "It is in retrograde - expect communication breakdowns and foul weather.",
+    "it is in retrograde - expect the unexpected.",
+    "The configuration of Scorpio's apex, combined with a diurnal focus on Neptune suggests that the undercarriage of Orion's belt will occur next Saturday at 11:47PM...which is to say, yes, Mercury is in retrograde. ",
+    "Maybe...(yeah :( )",
+    "Um. Uhhhhh. Yeaaaah. Yeah it is.",
+    "Retrograde...what a cool word!",
+];
+  
+const notInRetrogradeArray = [
+    "Phew - not today!",
+    "Nope definitely not, nooooo way, not today.",
+    "Non (that's French for no).",
+    "Nay!",
+    "Nooooooot today!",
+    "With the passing of the Solar plexus out of alignment with Saturn's left axis, Mercury is...NOT in retrograde.",
+    "Retrograde...what a cool word!",
+    "No! Thank goodness...",
+    "Yes...wait! No! Wait...",
+    "I can confidently say that Mercury is probably, most likely, not in retrograde.",
+    "It is............not."
+];
+
 export default function Card() {
+    const [inRetrograde, setInRetrograde] = useState(null)
+
     return (
         <div>
             <button>Click me</button>
