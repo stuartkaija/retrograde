6b9d4bd0ec728ac8b1443ffa90e062ad4d441abf
Stuart Kaija <stuartkaija@gmail.com>
2022-12-28 09:43:21 -0800

    initial commit, file set up, basic components outlined

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Card from './components/Card/Card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Is Mercury in Retrograde?</h1>
+			{/* card with button and answer below */}
+			<Card/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Card.scss';
+
+export default function Card() {
+    return (
+        <div>
+            <button>Click me</button>
+            <p>Answer goes here dynamically</p>
+        </div>
+    )
+}
