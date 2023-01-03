a70eef8bf59e45e32ffeaf72d2d2417d2dbd6b69
Stuart Kaija <stuartkaija@gmail.com>
2023-01-03 15:01:22 -0800

    style update

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import Card from './components/Card/Card';
 
 // background for application i.e. image pulled from Nasa goes in here
 
+// IF URL IS VIDEO, DON'T USE IT
+// imporove api call, some of the images are not attractive and also don't take up the whole screen
 
 function App() {
 	const [image, setImage] = useState<null | string>(null)
 
 	useEffect(() => {
+		console.log('useEffect running');
 		fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1`)
 		.then(res => res.json())
 		.then(data => {
