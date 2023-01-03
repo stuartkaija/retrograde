4eecc3768dd5d0a2f2d783e74ffc0152d92f14b7
Stuart Kaija <stuartkaija@gmail.com>
2023-01-03 13:33:10 -0800

    building functions to display random string, folder config

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import Card from './components/Card/Card';
 function App() {
 	return (
 		<div className="App">
-			<h1>Is Mercury in Retrograde?</h1>
-			{/* card with button and answer below */}
 			<Card/>
 		</div>
 	);
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,8 @@
-import {useState, useEffect} from 'react'
+import {useState} from 'react';
 import './Card.scss';
 
 // response arrays - ***this doesn't look very typescript-y right now, research how arrays (consisitng of strings) work in TS
-const inRetrogradeArray = [
+const inRetrogradeArray: readonly string[] = [
     "Oh no...ohhhhh no...(yes Mercury is in retrograde)",
     "Yeah. Better not go outside!",
     "Yes.",
@@ -14,7 +14,7 @@ const inRetrogradeArray = [
     "Retrograde...what a cool word!",
 ];
   
-const notInRetrogradeArray = [
+const notInRetrogradeArray: readonly string[] = [
     "Phew - not today!",
     "Nope definitely not, nooooo way, not today.",
     "Non (that's French for no).",
@@ -25,16 +25,33 @@ const notInRetrogradeArray = [
     "No! Thank goodness...",
     "Yes...wait! No! Wait...",
     "I can confidently say that Mercury is probably, most likely, not in retrograde.",
-    "It is............not."
+    "It is............not.",
 ];
 
 export default function Card() {
-    const [inRetrograde, setInRetrograde] = useState(null)
+    const [inRetrograde, setInRetrograde] = useState<boolean | null>(null);
+    const [sentence, setSentence] = useState<string>("");
+
+    function getRandomInt(max: number) {
+        return Math.floor(Math.random() * max)
+    }
+    
+    function getRandomString(array: readonly String[]) {
+        let randomNum = getRandomInt(array.length);
+        setSentence(inRetrogradeArray[randomNum]);
+        return array[randomNum];
+    }
+
+    // api call
 
     return (
-        <div>
-            <button>Click me</button>
-            <p>Answer goes here dynamically</p>
+        <div className="card">
+            <h1>Is Mercury in Retrograde?</h1>
+            {/* button click makes API call checking if mercury is in retrograde,
+            then depending on if it is or not, we update state of inRetrograde,
+            then update state of sentence */}
+            <button onClick={() => getRandomString(inRetrogradeArray)} className="card__button">Click me</button>
+            <p className="card__response">{sentence}</p>
         </div>
     )
 }
